63a6446e001453bd19b7647e231b5fad5c71c98b
LeoVacher <57897650+LeoVacher@users.noreply.github.com>
2024-01-30 17:24:44 +0100

    Update metalbar.py

--- _thermo/thermo/metalbar.py
+++ _thermo/thermo/metalbar.py
@@ -1,11 +1,17 @@
 
 from scipy.stats import norm 
+import numpy as np 
+import matplotlib.pyplot as plt
 
-x=np.arange(0,30,0.1)
-T= 15+10*norm.pdf(x, 15, 5)
-plt.plot(x, T ,c='darkred') 
-plt.plot(x[:-1], np.diff(T)/np.diff(x) ,c='darkblue') 
-plt.plot(x,0*np.ones(len(x)),c='k',linestyle='--')
-plt.ylabel("$T(x)$")
+x=np.arange(0,30,0.1) #x axis
+T= 10*norm.pdf(x, 15, 5) # put any function of $x$ you want here!
+Q= np.diff(T)/np.diff(x) # computing numerically the derivative
+
+# plotting figures
+
+plt.plot(x, T ,c='darkred') #T(x)
+plt.plot(x[:-1], Q ,c='darkblue') #Q(x)
+plt.plot(x,0*np.ones(len(x)),c='k',linestyle='--') # a bar labelling the zero
+plt.ylabel("$d T(x)/dx$")
 plt.xlabel("$x$")
 plt.show() 
